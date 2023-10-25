5ba7b1a3a8f335bceafad2b2e4c4999d372d7d8d
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-10-25 23:05:21 +0500

    added python code

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+# Foydalanuvchidan son qabul qilib, shu son miqdoricha Fibonachchi ketma-ketligidagi
+# sonlar ro'yxatni qaytaruvchi funksiya yozing.
+# Ta’rif: Har bir hadi o’zidan oldingi ikkita hadning yig’indisiga teng bo’lgan ketma-ketlik
+# Fibonachchi ketma-ketligi deyiladi. Bunda boshlang’ish had ko’pincha 1 deb olinadi.
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,👇🏻
+
+def fibonacci(n):
+    sonlar = []
+    for x in range(n):
+        if x == 0 or x == 1:
+            sonlar.append(1)
+        else:
+            sonlar.append(sonlar[x - 1] + sonlar[x - 2])
+    return sonlar
+
+
+print(fibonacci(20))
